ffb15bbf08dd7dbb71334cd3f6c277416dfcd15d
Kreoniz <kreoniz@mail.ru>
2024-11-15 10:39:41 +0600

    chore: create an api call to get images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import "./api";
+import { useEffect, useState } from "react";
+import { getMarsRoverPhotos } from "./api";
+import { IPhotos } from "./types";
 
 export function App() {
-  return <div className="text-xl font-bold">App</div>;
+  const [photos, setPhotos] = useState<IPhotos | null>(null);
+
+  const CARD_AMOUNT = 10;
+  const rover_name = "curiosity";
+  useEffect(() => {
+    getMarsRoverPhotos(rover_name, CARD_AMOUNT)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPhotos(data.photos.slice(10));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="text-xl font-bold">App</div>
+      <div>
+        {photos ? <div>{JSON.stringify(photos)}</div> : <div>Loading...</div>}
+      </div>
+    </div>
+  );
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,15 @@
 const API = import.meta.env.VITE_API;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-console.log(API);
+export async function getMarsRoverPhotos(
+  rover: "curiosity" | "opportunity" | "spirit",
+  count: number,
+) {
+  const page = Math.ceil(count / 25);
+
+  const data = await fetch(
+    `${API}/mars-photos/api/v1/rovers/${rover}/photos?page=${page}&sol=1000&api_key=${API_KEY}`,
+  );
+
+  return data;
+}
