7ddba120d764cc51491cb3b174d5d012be561432
Kreoniz <kreoniz@mail.ru>
2024-11-18 12:51:29 +0600

    feat: add information modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { getMarsRoverPhotos, getMarsRoverInfo } from "./api";
 import { IPhoto } from "./types";
 import { Card } from "./components/Card";
+import { QuestionIcon, CrossIcon } from "./components/icons";
 
 export function App() {
   const [photos, setPhotos] = useState<IPhoto[] | null>(null);
   const [clickedItems, setClickedItems] = useState<number[]>([]);
   const [bestScore, setBestScore] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const CARD_AMOUNT = 10;
   useEffect(() => {
@@ -35,6 +37,18 @@ export function App() {
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isModalOpen]);
 
   function shuffle(array: IPhoto[] | null) {
     if (!array) return [];
@@ -67,13 +81,19 @@ export function App() {
   }
 
   return (
-    <div className="py-4 px-2 md:px-4">
+    <div className="py-4 px-2 md:px-4 relative h-full w-full">
       <header className="flex justify-between items-center mb-4">
         <h2 className="text-3xl font-bold text-center">Rovertastic!</h2>
 
-        <div className="font-bold">
-          <div>Best score: {bestScore}</div>
-          <div>Current score: {clickedItems.length}</div>
+        <div className="font-bold flex gap-3 items-center">
+          <div className="text-right">
+            <div>Best score: {bestScore}</div>
+            <div>Current score: {clickedItems.length}</div>
+          </div>
+
+          <button type="button" onClick={() => setIsModalOpen(true)}>
+            <QuestionIcon height="45" width="45" />
+          </button>
         </div>
       </header>
 
@@ -98,6 +118,35 @@ export function App() {
           <div>Loading...</div>
         )}
       </div>
+
+      <dialog
+        open={isModalOpen}
+        className="absolute min-w-screen w-full min-h-screen h-full bg-gray-950/60 top-0 left-0"
+        id="modal"
+      >
+        <div className="flex items-center justify-center w-screen h-screen">
+          <div className="relative w-[320px] bg-white shadow-black rounded-2xl py-8 px-2">
+            <h2 className="text-2xl font-bold text-center mb-2">Information</h2>
+
+            <div className="flex flex-col gap-2 text-center">
+              <p>
+                You should click on a card you haven't clicked in the current
+                round yet
+              </p>
+              <p>After each click the cards are shuffled!</p>
+              <p>To win, you should click on all the cards</p>
+            </div>
+
+            <button
+              className="absolute top-0 right-0 p-6"
+              type="button"
+              onClick={() => setIsModalOpen(false)}
+            >
+              <CrossIcon height="40" width="40" />
+            </button>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
---
+++ src/components/icons/index.ts
@@ -0,0 +1,2 @@
+export { QuestionIcon } from "./QuestionIcon";
+export { CrossIcon } from "./CrossIcon";
